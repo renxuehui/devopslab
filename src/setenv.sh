98c1d299ada472768b4d5e7bf3c6fdb38555b029
renxuehui <48955149+renxuehui@users.noreply.github.com>
2019-12-07 15:01:47 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,8 +1,8 @@
-export GITACCOUNT=
+export GITACCOUNT=renxuehui1983@163.com
 
-export MYCLUSTER=
-export KUBECONFIG=
+export MYCLUSTER=tektonknativebeijing43
+export KUBECONFIG=/tmp/ic/AA/.bluemix/plugins/container-service/clusters/tektonknativebeijing43/kube-config-tok04-tektonknativebeijing43.yml
 
-export NAMESPACE=
-export EMAIL=
-export INGRESS=
+export NAMESPACE=xuehui
+export EMAIL=renxuehui1983@163.com
+export INGRESS=tektonknativebeijing43-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud
